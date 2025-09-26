8d7715dfa695d032bde352b632f7a7c51574da80
kurdukovr2564@gmail.com <Roman@MacBook-Air-4.local>
2025-09-26 21:44:50 +0300

    fixed getTonBalance

--- src/shared/utils/getTonBalance.ts
+++ src/shared/utils/getTonBalance.ts
@@ -1,28 +1,27 @@
+import { Address } from "@ton/core";
+
 export async function getTonBalance(address: string) {
   try {
+    // Конвертируем адрес в правильный формат
+    const walletAddress = Address.parse(address);
+
     const response = await fetch(
       "https://toncenter.com/api/v2/getWalletInformation",
       {
-        method: "GET",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          address: address,
+          address: walletAddress.toString(),
         }),
       }
     );
 
     const data = await response.json();
-
-    if (data.ok && data.result) {
-      const balanceTon = (
-        BigInt(data.result.balance) / BigInt(1000000000)
-      ).toString();
-      return balanceTon;
-    }
-
-    throw new Error("Failed to get balance");
+    return data.result
+      ? (BigInt(data.result.balance) / BigInt(1000000000)).toString()
+      : "0";
   } catch (error) {
     console.error("Error:", error);
     return "0";
